486e43a96cd04a1e37b8d00efdfafc9649d905d8
Sesso Kosga <kosgasesso@gmail.com>
2023-06-07 15:25:18 +0100

    Fixed some conversion issues

--- src/convert.rs
+++ src/convert.rs
@@ -35,34 +35,42 @@ pub fn less_than_100(number: usize) -> (String, String, String) {
     let mut result_katakana = String::new();
     let mut result_romaji = String::new();
     let mut result_kanji = String::new();
-    if number < 10000 {
-        // Decimals
-        if number < 100 && number > 10 {
-            let decimal = number / 10;
-            result_romaji += UNITS_ROMAJI[decimal];
-            result_romaji += " ";
+
+    // Decimals
+    if number < 100 {
+        let decimal = number / 10;
+
+        // Prevent ichi juu
+        if decimal > 0 {
+            if decimal > 1 {
+                result_romaji += UNITS_ROMAJI[decimal];
+                result_romaji += " ";
+
+                result_kanji += UNITS_KANJI[decimal];
+
+                result_katakana += UNITS_KATAKANA[decimal];
+                result_katakana += " ";
+            }
+
             result_romaji += UNITS_ROMAJI[10];
             result_romaji += " ";
 
-            result_kanji += UNITS_KANJI[decimal];
             result_kanji += UNITS_KANJI[10];
             result_romaji += " ";
 
-            result_katakana += UNITS_KATAKANA[decimal];
-            result_katakana += " ";
             result_katakana += UNITS_KATAKANA[10];
             result_katakana += " ";
-
-            // Units
-            let left = number - decimal * 10;
-            if left > 0 {
-                let res = less_than_11(left);
-                result_kanji += &res.0;
-                result_katakana += &res.1;
-                result_romaji += &res.2;
-            }
+        }
+        // Units
+        let left = number - decimal * 10;
+        if left > 0 {
+            let res = less_than_11(left);
+            result_kanji += &res.0;
+            result_katakana += &res.1;
+            result_romaji += &res.2;
         }
     }
+
     (result_kanji, result_katakana, result_romaji)
 }
 
@@ -73,29 +81,28 @@ pub fn less_than_1000(number: usize) -> (String, String, String) {
 
     if number < 1000 {
         let hundred = number / 100;
-        
-        // Prevent ichi hyaku
-        if hundred >1{
-            result_romaji += UNITS_ROMAJI[hundred];
+        // Prevent "hyaku juu" when got "10" as input
+        if hundred > 0 {
+            // Prevent ichi hyaku
+            if hundred > 1 {
+                result_romaji += UNITS_ROMAJI[hundred];
+                result_romaji += " ";
+
+                result_kanji += UNITS_KANJI[hundred];
+
+                result_katakana += UNITS_KATAKANA[hundred];
+                result_katakana += " ";
+            }
+
+            result_romaji += "hyaku";
             result_romaji += " ";
 
-            result_kanji += UNITS_KANJI[hundred];
+            result_kanji += "百";
+            result_romaji += " ";
 
-            result_katakana += UNITS_KATAKANA[hundred];
+            result_katakana += "ヒャク";
             result_katakana += " ";
         }
-
-        result_romaji += "hyaku";
-        result_romaji += " ";
-
-        
-        result_kanji += "百";
-        result_romaji += " ";
-
-        
-        result_katakana += "ヒャク";
-        result_katakana += " ";
-
         let left = number - hundred * 100;
         if left > 0 {
             let res = less_than_100(number % 100);
@@ -107,3 +114,21 @@ pub fn less_than_1000(number: usize) -> (String, String, String) {
 
     (result_kanji, result_katakana, result_romaji)
 }
+
+// pub fn less_than_10k(number: usize) -> (String, String, String) {
+//     let mut result_katakana = String::new();
+//     let mut result_romaji = String::new();
+//     let mut result_kanji = String::new();
+
+//     if number < 1000 {
+
+//     }
+
+//     let left = number - hundred * 100;
+//     if left > 0 {
+//         let res = less_than_100(number % 100);
+//         result_kanji += &res.0;
+//         result_katakana += &res.1;
+//         result_romaji += &res.2;
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod convert;
 fn main() {
-    let num = convert::less_than_1000(999);
+    let num = convert::less_than_1000(101);
     println!("1  =>  {}  =>  {}  =>  {}",num.0,num.1,num.2);
 
 }
