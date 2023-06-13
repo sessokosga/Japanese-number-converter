8a78b3ea6bacaecfac811485f69aa0ad9532c1f3
Sesso Kosga <kosgasesso@gmail.com>
2023-06-14 00:42:47 +0100

    Removed main.rs

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let result = japanese_number_converter::
-                        JapaneseNumber::convert(1844_6744_0737_0955_1615);
-    println!("Result : {}  =>  {}  =>  {}  =>  {}",
-        result.arabiasuji(),result.kanji(),result.katakana(),result.romaji());
-}
